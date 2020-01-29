b24f6ac2fde12bbdccf094f1d69c4964fc8b20ce
Matt Martindale <martindalematt18@yahoo.com>
2020-01-28 21:53:35 -0700

    created MVC structure and segues

---
+++ SwiftVocabulary/Model/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  SwiftVocabulary
+//
+//  Created by Matthew Martindale on 1/28/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftVocabulary/View Controllers/DefinitionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DefinitionViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Matthew Martindale on 1/28/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
